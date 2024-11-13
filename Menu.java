bf33766b6afc3a149ec587538a85fbc15ed91998
Rikiments <142755990+Rikiments@users.noreply.github.com>
2024-11-13 17:14:36 -0300

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,15 @@
+public class Menu {
+	
+	public String telaInicial(){
+		String txt;
+		txt="\t\t***MACK THEATHER***\n";
+		txt+="1) Cadastrar Espetaculo\n";
+		txt+="2) Cadastrar Cliente\n";
+		txt+="3) Compra de Entradas\n";
+		txt+="4) Sair\n";
+		txt+="Selecione uma opção:";
+		return txt;
+	}
+	
+	
+}
