3c3f2ea870161ab4cf484b6d9400d4b167ecce29
Rikiments <142755990+Rikiments@users.noreply.github.com>
2024-11-14 08:51:54 -0300

    Update EntradaProfessor.java

--- EntradaProfessor.java
+++ EntradaProfessor.java
@@ -1,15 +1,10 @@
-
 public class EntradaProfessor extends Entrada {
-	private double preco;
-
-    public EntradaProfessor(int numeroDoAssento, double preco) {
-        super(numeroDoAssento);
-        this.preco = preco;
+    public EntradaProfessor(int assento, double precoBase) {
+        super(assento, precoBase);
     }
 
     @Override
     public double calculaValor() {
-        return preco * 0.4;
+        return precoBase * 0.6; // 40% de desconto
     }
-
 }
