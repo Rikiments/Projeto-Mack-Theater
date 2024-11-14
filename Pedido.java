05de2607c943f63fa5ba6ab8f84eb325ea6cc070
Rikiments <142755990+Rikiments@users.noreply.github.com>
2024-11-14 08:50:06 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 
 public class Pedido {
-	private ArrayList<Entrada> entradas = new ArrayList<>();
+	private ArrayList<Entrada> entradas = new ArrayList<>(); // Lista que guarda todas as entradas (ingressos) de um pedido específico.
 
-    public Pedido() {}
 
-    public void adicionaEntrada(Entrada entrada) {
+    public Pedido() {} // construtor padrao 
+
+    public void adicionaEntrada(Entrada entrada) { // Adiciona uma entrada (ingresso) ao pedido.
         entradas.add(entrada);
     }
 
-    public double calculaValorTotal() {
+    public double calculaValorTotal() { //  Calcula o valor total do pedido, somando o valor de todas as entra
         double total = 0;
-        for (Entrada entrada : entradas) {
+        for (Entrada entrada : entradas) { // Percorre cada entrada na lista e chama 'calculaValor' para obter o preço
             total += entrada.calculaValor();
         }
         return total;
