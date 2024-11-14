4440e789ad38980f425b5463b05feaeee9e4f01b
Rikiments <142755990+Rikiments@users.noreply.github.com>
2024-11-14 08:51:02 -0300

    Update EntradaInteira.java

--- EntradaInteira.java
+++ EntradaInteira.java
@@ -1,14 +1,10 @@
-
 public class EntradaInteira extends Entrada {
-	private double preco;
-
-    public EntradaInteira(int numeroDoAssento, double preco) {
-        super(numeroDoAssento);
-        this.preco = preco;
+    public EntradaInteira(int assento, double precoBase) {
+        super(assento, precoBase);
     }
 
     @Override
     public double calculaValor() {
-        return preco;
+        return precoBase; // Preço integral
     }
 }
